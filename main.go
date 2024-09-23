4fdef291b57987a14ee4fb0bf8fcae43a7f7784f
HEX9CF <hex9cf@aliyun.com>
2024-09-23 10:30:39 +0800

    Import gin

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!", s)
+	ginServer := gin.Default()
 
-	for i := 1; i <= 5; i++ {
-		fmt.Println("i =", 100/i)
+	ginServer.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"msg": "STUOJ后端服务启动成功！",
+		})
+	})
+
+	err := ginServer.Run()
+	if err != nil {
+		return
 	}
 }
