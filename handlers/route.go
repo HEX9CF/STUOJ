5c0748680ea182d20b18b8021acb7edb9e52aa68
HEX9CF <hex9cf@aliyun.com>
2024-09-29 15:02:38 +0800

    Change response data format

--- handlers/route.go
+++ handlers/route.go
@@ -3,23 +3,24 @@ package handlers
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
-
 )
-
-
 
 func InitRoute() {
 	// index
 	ginServer.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"msg": "STUOJ后端服务启动成功！",
+			"code": 1,
+			"msg":  "STUOJ后端服务启动成功！",
+			"data": nil,
 		})
 	})
 
 	// 404
 	ginServer.NoRoute(func(c *gin.Context) {
 		c.JSON(http.StatusNotFound, gin.H{
-			"msg": "404 Not Found",
+			"code": 0,
+			"msg":  "404 Not Found",
+			"data": nil,
 		})
 	})
 	InitUserRoute()
@@ -31,12 +32,12 @@ func InitRoute() {
 }
 
 func InitUserRoute() {
-	userRoute:=ginServer.Group("/user")
+	userRoute := ginServer.Group("/user")
 	{
-		userRoute.POST("/login",UserLogin)
-		userRoute.POST("/register",UserRegister)
-		userRoute.POST("/logout",UserLogout)
-		userRoute.POST("/data",UserData)
+		userRoute.POST("/login", UserLogin)
+		userRoute.POST("/register", UserRegister)
+		userRoute.POST("/logout", UserLogout)
+		userRoute.POST("/data", UserData)
 	}
 
 }
