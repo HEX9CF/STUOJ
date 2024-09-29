09003039b3d3c04d644c8d09731991bc069f4385
ArtdragonXoX <1262922587@qq.com>
2024-09-29 14:05:12 +0800

    文件架构

--- model/user.go
+++ model/user.go
@@ -1,1 +1,30 @@
 package model
+
+type Role uint8
+
+const (
+	RoleBanned  Role = 1
+	RoleUser    Role = 2
+	RoleAdmin   Role = 3
+	RoleRoot    Role = 4
+)
+
+func (r Role) String() string {
+	switch r {
+	case RoleBanned:
+		return "banned"
+	case RoleUser:
+		return "user"
+	case RoleAdmin:
+		return "admin"
+	case RoleRoot:
+		return "root"
+	default:
+		return "unknown"
+	}
+}
+
+type LoginUserReq struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
