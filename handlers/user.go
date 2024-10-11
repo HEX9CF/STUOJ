8e687184486c01f95a20e3cd1c063cdac24db872
HEX9CF <hex9cf@aliyun.com>
2024-10-11 15:35:42 +0800

    Add ReqUserRegister

--- handlers/user.go
+++ handlers/user.go
@@ -1,12 +1,46 @@
 package handlers
 
 import (
+	"STUOJ/model"
 	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
 )
 
-func UserLogin(c *gin.Context) {}
+type ReqUserLogin struct {
+	Password string `json:"password" binding:"required"`
+	Email    string `json:"email" binding:"required"`
+}
+
+func UserLogin(c *gin.Context) {
+
+}
+
+type ReqUserRegister struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+	Email    string `json:"email" binding:"required"`
+}
 
 func UserRegister(c *gin.Context) {
+	var req ReqUserRegister
+
+	err := c.ShouldBindBodyWithJSON(&req)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, model.Response{
+			Code: 0,
+			Msg:  err.Error(),
+			Data: nil,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, model.Response{
+		Code: 1,
+		Msg:  "OK",
+		Data: req,
+	})
 }
 
 func UserLogout(c *gin.Context) {
