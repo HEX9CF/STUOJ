dcea97f46840273f073ea8d1e4b3d9d2a32f40d8
HEX9CF <hex9cf@aliyun.com>
2024-10-12 16:02:36 +0800

    Add jwt token utils

---
+++ utils/token.go
@@ -0,0 +1,73 @@
+package utils
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func GenerateToken(id uint64) (string, error) {
+	lifespan, err := strconv.Atoi(os.Getenv("TOKEN_EXPIRE"))
+	if err != nil {
+		return "", err
+	}
+
+	claims := jwt.MapClaims{}
+	claims["authorized"] = true
+	claims["uid"] = id
+	claims["exp"] = time.Now().Add(time.Hour * time.Duration(lifespan)).Unix()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(os.Getenv("API_SECRET")))
+}
+
+func ExtractToken(c *gin.Context) string {
+	bearerToken := c.GetHeader("Authorization")
+	if len(strings.Split(bearerToken, " ")) == 2 {
+		return strings.Split(bearerToken, " ")[1]
+	}
+	return ""
+}
+
+func VerifyToken(c *gin.Context) error {
+	tokenString := ExtractToken(c)
+	_, err := jwt.Parse(tokenString, func(t *jwt.Token) (any, error) {
+		_, ok := t.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", t.Header["alg"])
+		}
+		return []byte(os.Getenv("API_SECRET")), nil
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func ExtractTokenUid(c *gin.Context) (uint64, error) {
+	tokenString := ExtractToken(c)
+	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (any, error) {
+		_, ok := t.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", t.Header["alg"])
+		}
+		return []byte(os.Getenv("API_SECRET")), nil
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		uid, err := strconv.ParseUint(fmt.Sprintf("%.0f", claims["uid"]), 10, 64)
+		if err != nil {
+			return 0, err
+		}
+		return uint64(uid), nil
+	}
+	return 0, nil
+}
