37769e3b495b5435c801d72fd007b3510c9270f0
HEX9CF <hex9cf@aliyun.com>
2024-10-23 10:37:14 +0800

    Update problem model

--- model/problem.go
+++ model/problem.go
@@ -2,6 +2,31 @@ package model
 
 import "time"
 
+type Status uint8
+
+// 状态：0 作废，1 公开，2 出题中，3 调试中
+const (
+	StatusInvalid   Status = 0
+	StatusPublic    Status = 1
+	StatusEditing   Status = 2
+	StatusDebugging Status = 3
+)
+
+func (s Status) String() string {
+	switch s {
+	case StatusInvalid:
+		return "invalid"
+	case StatusPublic:
+		return "public"
+	case StatusEditing:
+		return "editing"
+	case StatusDebugging:
+		return "debugging"
+	default:
+		return "unknown"
+	}
+}
+
 // 题目
 type Problem struct {
 	Id           uint64    `json:"id"`
@@ -16,6 +41,7 @@ type Problem struct {
 	SampleInput  string    `json:"sample_input"`
 	SampleOutput string    `json:"sample_output"`
 	Hint         string    `json:"hint"`
+	Status       Status    `json:"status"`
 	CreateTime   time.Time `json:"create_time"`
 	UpdateTime   time.Time `json:"update_time"`
 }
