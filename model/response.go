9552ae33126f78fea4e6c0d09904e4a9d73ecb86
HEX9CF <hex9cf@aliyun.com>
2024-10-24 19:35:05 +0800

    Update response model

--- model/response.go
+++ model/response.go
@@ -1,8 +1,26 @@
 package model
 
+type ResponseCode uint8
+
+const (
+	ResponseCodeError   ResponseCode = 0
+	ResponseCodeSuccess ResponseCode = 1
+)
+
+func (c ResponseCode) String() string {
+	switch c {
+	case ResponseCodeError:
+		return "错误"
+	case ResponseCodeSuccess:
+		return "成功"
+	default:
+		return "未知状态"
+	}
+}
+
 // http响应体
 type Response struct {
-	Code int         `json:"code"` // 0 失败，1 成功
-	Msg  string      `json:"msg"`
-	Data interface{} `json:"data"`
+	Code ResponseCode `json:"code"`
+	Msg  string       `json:"msg"`
+	Data interface{}  `json:"data"`
 }
