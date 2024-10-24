7a3be4101f7057d2f0dfed6714fb98eb601a95df
HEX9CF <hex9cf@aliyun.com>
2024-10-24 20:33:10 +0800

    Update judgement model

--- model/judgement.go
+++ model/judgement.go
@@ -5,9 +5,14 @@ type JudgementStatus uint8
 
 // 单个评测点结果
 type Judgement struct {
-	Id           uint64          `json:"id"`
-	SubmissionID uint64          `json:"submission_id"`
-	Status       JudgementStatus `json:"status"`
-	Time         float64         `json:"time"`
-	Memory       uint64          `json:"memory"`
+	Id            uint64  `json:"id"`
+	SubmissionId  uint64  `json:"submission_id"`
+	TestPointId   uint64  `json:"test_point_id"`
+	Time          float64 `json:"time"`
+	Memory        uint64  `json:"memory"`
+	Stdout        string  `json:"stdout"`
+	Stderr        string  `json:"stderr"`
+	CompileOutput string  `json:"compile_output"`
+	Message       string  `json:"message"`
+	Status        uint64  `json:"status"`
 }
