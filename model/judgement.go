ba12f6c93cf7c2dbccf509b741f37903c83c656e
HEX9CF <hex9cf@aliyun.com>
2024-10-25 10:32:16 +0800

    Use SubmissionStatus as Status's data type of judgement model

--- model/judgement.go
+++ model/judgement.go
@@ -1,18 +1,15 @@
 package model
 
-// 评测状态
-type JudgementStatus uint8
-
 // 单个评测点结果
 type Judgement struct {
-	Id            uint64  `json:"id"`
-	SubmissionId  uint64  `json:"submission_id"`
-	TestPointId   uint64  `json:"test_point_id"`
-	Time          float64 `json:"time"`
-	Memory        uint64  `json:"memory"`
-	Stdout        string  `json:"stdout"`
-	Stderr        string  `json:"stderr"`
-	CompileOutput string  `json:"compile_output"`
-	Message       string  `json:"message"`
-	Status        uint64  `json:"status"`
+	Id            uint64           `json:"id"`
+	SubmissionId  uint64           `json:"submission_id"`
+	TestPointId   uint64           `json:"test_point_id"`
+	Time          float64          `json:"time"`
+	Memory        uint64           `json:"memory"`
+	Stdout        string           `json:"stdout"`
+	Stderr        string           `json:"stderr"`
+	CompileOutput string           `json:"compile_output"`
+	Message       string           `json:"message"`
+	Status        SubmissionStatus `json:"status"`
 }
