94284bd9868d5af1bda0a6bb997472bff4b78451
HEX9CF <hex9cf@aliyun.com>
2024-10-28 16:10:54 +0800

    Update judgement model

--- model/judgement.go
+++ model/judgement.go
@@ -7,9 +7,9 @@ type Judgement struct {
 	TestPointId   uint64       `json:"test_point_id"`
 	Time          float64      `json:"time"`
 	Memory        uint64       `json:"memory"`
-	Stdout        string       `json:"stdout"`
-	Stderr        string       `json:"stderr"`
-	CompileOutput string       `json:"compile_output"`
-	Message       string       `json:"message"`
+	Stdout        string       `json:"stdout,omitempty"`
+	Stderr        string       `json:"stderr,omitempty"`
+	CompileOutput string       `json:"compile_output,omitempty"`
+	Message       string       `json:"message,omitempty"`
 	Status        SubmitStatus `json:"status"`
 }
