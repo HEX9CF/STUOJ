92fea27158560aee2c61ef88bd565c91afefb23b
HEX9CF <hex9cf@aliyun.com>
2024-10-30 14:10:24 +0800

    Update record model add omitempty tag

--- model/record.go
+++ model/record.go
@@ -2,6 +2,6 @@ package model
 
 // 提交记录（包含提交信息和评测点结果）
 type Record struct {
-	Submission Submission  `json:"submission"`
-	Judgements []Judgement `json:"judgements"`
+	Submission Submission  `json:"submission,omitempty"`
+	Judgements []Judgement `json:"judgements,omitempty"`
 }
