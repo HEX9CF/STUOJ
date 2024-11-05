841d5edbe7f7a1ca74011529451e1f682de7d177
HEX9CF <hex9cf@aliyun.com>
2024-11-05 18:18:54 +0800

    Add ResetDatabase service

--- db/init.go
+++ db/init.go
@@ -2,7 +2,6 @@ package db
 
 import (
 	"STUOJ/conf"
-	"database/sql"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
@@ -11,25 +10,19 @@ import (
 	"time"
 )
 
-var (
-	SqlDb *sql.DB
-	Db    *gorm.DB
-)
-
 // 初始化数据库
 func InitDatabase() error {
 	var err error
 	config := conf.Conf.Datebase
-
+	myLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold: time.Second,
+			LogLevel:      logger.Info,
+			Colorful:      true,
+		})
 	gormConfig := &gorm.Config{
-		Logger: logger.New(
-			log.New(os.Stdout, "\r\n", log.LstdFlags),
-			logger.Config{
-				SlowThreshold: time.Second,
-				LogLevel:      logger.Info,
-				Colorful:      true,
-			},
-		),
+		Logger: myLogger,
 	}
 
 	dsn := config.User + ":" + config.Pwd + "@tcp(" + config.Host + ":" + config.Port + ")/" + config.Name + "?charset=utf8mb4&parseTime=True&loc=Local"
