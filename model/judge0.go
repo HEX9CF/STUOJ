0f964bdebc7fe62d85905c82689517e085baa036
ArtdragonXoX <1262922587@qq.com>
2024-11-08 10:55:24 +0800

    judge提交结构体增加omitempty字段。

--- model/judge0.go
+++ model/judge0.go
@@ -3,10 +3,10 @@ package model
 type JudgeSubmission struct {
 	SourceCode     string  `json:"source_code"`
 	LanguageId     uint64  `json:"language_id"`
-	Stdin          string  `json:"stdin"`
-	ExpectedOutput string  `json:"expected_output"`
-	CPUTimeLimit   float64 `json:"cpu_time_limit"`
-	MemoryLimit    uint64  `json:"memory_limit"`
+	Stdin          string  `json:"stdin,omitempty"`
+	ExpectedOutput string  `json:"expected_output,omitempty"`
+	CPUTimeLimit   float64 `json:"cpu_time_limit,omitempty"`
+	MemoryLimit    uint64  `json:"memory_limit,omitempty"`
 }
 
 type JudgeSubmissionResult struct {
