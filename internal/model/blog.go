983f516f72a91eae7871be5e00658a83eeaf7f71
HEX9CF <hex9cf@aliyun.com>
2024-11-20 10:46:06 +0800

    Remove tags from BlogData model

--- internal/model/blog.go
+++ internal/model/blog.go
@@ -2,9 +2,8 @@ package model
 
 import "STUOJ/internal/entity"
 
-// 博客数据（博客+标签+评论）
+// 博客数据（博客+评论）
 type BlogData struct {
 	Blog     entity.Blog      `json:"blog,omitempty"`
-	Tags     []entity.Tag     `json:"tags,omitempty"`
 	Comments []entity.Comment `json:"comments,omitempty"`
 }
