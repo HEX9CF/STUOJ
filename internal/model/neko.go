082b1dbf066559ccf115f87532c8b334d76a59f8
HEX9CF <hex9cf@aliyun.com>
2024-12-10 18:34:07 +0800

    Add mapstructure tag to neko-acm models

--- internal/model/neko.go
+++ internal/model/neko.go
@@ -2,14 +2,14 @@ package model
 
 // 题目
 type NekoProblem struct {
-	Title        string   `json:"title,omitempty"`
-	Description  string   `json:"description,omitempty"`
-	Input        string   `json:"input,omitempty"`
-	Output       string   `json:"output,omitempty"`
-	SampleInput  string   `json:"sample_input,omitempty"`
-	SampleOutput string   `json:"sample_output,omitempty"`
-	Hint         string   `json:"hint,omitempty"`
-	Tags         []string `json:"tags,omitempty"`
+	Title        string   `json:"title,omitempty" mapstructure:"title,omitempty"`
+	Description  string   `json:"description,omitempty" mapstructure:"description,omitempty"`
+	Input        string   `json:"input,omitempty" mapstructure:"input,omitempty"`
+	Output       string   `json:"output,omitempty" mapstructure:"output,omitempty"`
+	SampleInput  string   `json:"sample_input,omitempty" mapstructure:"sample_input,omitempty"`
+	SampleOutput string   `json:"sample_output,omitempty" mapstructure:"sample_output,omitempty"`
+	Hint         string   `json:"hint,omitempty" mapstructure:"hint,omitempty"`
+	Tags         []string `json:"tags,omitempty" mapstructure:"tags,omitempty"`
 }
 
 // 题目说明
@@ -27,10 +27,10 @@ type NekoProblemInstruction struct {
 
 // 测试用例
 type NekoTestcase struct {
-	TestInput         string `json:"test_input,omitempty"`
-	TestOutput        string `json:"test_output,omitempty"`
-	InputExplanation  string `json:"input_explanation,omitempty"`
-	OutputExplanation string `json:"output_explanation,omitempty"`
+	TestInput         string `json:"test_input,omitempty" mapstructure:"test_input,omitempty"`
+	TestOutput        string `json:"test_output,omitempty" mapstructure:"test_output,omitempty"`
+	InputExplanation  string `json:"input_explanation,omitempty" mapstructure:"input_explanation,omitempty"`
+	OutputExplanation string `json:"output_explanation,omitempty" mapstructure:"output_explanation,omitempty"`
 }
 
 // 测试用例说明
@@ -48,9 +48,9 @@ type NekoTestcaseInstruction struct {
 
 // 题解
 type NekoSolution struct {
-	Language    string `json:"language,omitempty"`
-	SourceCode  string `json:"source_code,omitempty"`
-	Explanation string `json:"explanation,omitempty"`
+	Language    string `json:"language,omitempty" mapstructure:"language,omitempty"`
+	SourceCode  string `json:"source_code,omitempty" mapstructure:"source_code,omitempty"`
+	Explanation string `json:"explanation,omitempty" mapstructure:"explanation,omitempty"`
 }
 
 // 题解说明
@@ -68,7 +68,7 @@ type NekoSolutionInstruction struct {
 }
 
 type NekoResponse struct {
-	Code int         `json:"code"`
-	Msg  string      `json:"msg"`
-	Data interface{} `json:"data"`
+	Code int                    `json:"code"`
+	Msg  string                 `json:"msg"`
+	Data map[string]interface{} `json:"data"`
 }
