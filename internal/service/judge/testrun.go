b111c6ba093f42f8bdd5eb0929209a5c8cf3e690
HEX9CF <hex9cf@aliyun.com>
2024-12-18 17:16:34 +0800

    Update testrun

--- internal/service/judge/testrun.go
+++ internal/service/judge/testrun.go
@@ -4,39 +4,54 @@ import (
 	"STUOJ/external/judge0"
 	"STUOJ/internal/entity"
 	"STUOJ/internal/model"
-	"errors"
 	"log"
 	"strconv"
 )
 
 func TestRun(s entity.Submission, stdin string) (entity.Judgement, error) {
-	js := model.JudgeSubmission{
-		SourceCode: s.SourceCode,
-		LanguageId: s.LanguageId,
-		Stdin:      stdin,
-	}
+	var err error
+	j := entity.Judgement{}
 
-	res, err := judge0.Submit(js)
+	// 初始化评测点评测对象
+	js := model.JudgeSubmission{
+		SourceCode:     s.SourceCode,
+		LanguageId:     s.LanguageId,
+		Stdin:          stdin,
+		ExpectedOutput: "",
+		CPUTimeLimit:   2,
+		MemoryLimit:    102400,
+	}
+	//log.Println(js)
+
+	// 发送评测点评测请求（等待评测结果）
+	result, err := judge0.Submit(js)
 	if err != nil {
 		log.Println(err)
-		return entity.Judgement{}, errors.New("提交失败")
+		j.Status = entity.JudgeStatusIE
+		return j, err
 	}
+	//log.Println(result)
 
 	// 解析时间
 	time := float64(0)
-	if res.Time != "" {
-		time, err = strconv.ParseFloat(res.Time, 64)
+	if result.Time != "" {
+		time, err = strconv.ParseFloat(result.Time, 64)
 		if err != nil {
 			log.Println(err)
-			return entity.Judgement{}, errors.New("时间解析失败")
+			j.Status = entity.JudgeStatusIE
+			return j, err
 		}
 	}
 
-	j := entity.Judgement{
-		Stdout: res.Stdout,
-		Time:   time,
-		Memory: uint64(res.Memory),
-	}
+	// 更新评测点结果
+	j.Time = time
+	j.Memory = uint64(result.Memory)
+	j.Stdout = result.Stdout
+	j.Stderr = result.Stderr
+	j.CompileOutput = result.CompileOutput
+	j.Message = result.Message
+	j.Status = entity.JudgeStatus(result.Status.Id)
+	//log.Println(j)
 
 	return j, nil
 }
